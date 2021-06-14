6c231712981cfaae0a377bf2ad3e0cf9fa2f729b
rudnovd <rudnov.d@gmail.com>
2021-06-14 22:05:25 +0500

    style: apply new style rules

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { initializeApp } from 'firebase/app'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
@@ -9,19 +9,19 @@ const firebaseConfig = {
   messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.VUE_APP_FIREBASE_APP_ID,
   databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
-};
+}
 
-initializeApp(firebaseConfig);
+initializeApp(firebaseConfig)
 
-const auth = getAuth();
+const auth = getAuth()
 onAuthStateChanged(auth, (user) => {
   if (user) {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/firebase.User
-    const uid = user.uid;
+    const uid = user.uid
     // ...
   } else {
     // User is signed out
     // ...
   }
-});
+})
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import { Quasar } from "quasar";
-import quasarUserOptions from "./quasar-user-options";
-import "./firebase";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
+import './firebase'
 
-createApp(App)
-  .use(Quasar, quasarUserOptions)
-  .use(store)
-  .use(router)
-  .mount("#app");
+createApp(App).use(Quasar, quasarUserOptions).use(store).use(router).mount('#app')

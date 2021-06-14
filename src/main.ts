8b92a01f31bdfad8c755abd1904b9d0d2878af70
rudnovd <rudnov.d@gmail.com>
2021-06-14 15:35:52 +0500

    feat: init firebase

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from "./router";
 import store from "./store";
 import { Quasar } from "quasar";
 import quasarUserOptions from "./quasar-user-options";
+import "./firebase";
 
 createApp(App)
   .use(Quasar, quasarUserOptions)

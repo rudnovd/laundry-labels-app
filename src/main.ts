caa933640d26260e1d5b0992eaa8fda81c06dd3f
rudnovd <rudnov.d@gmail.com>
2021-07-11 22:36:01 +0500

    refactor(main): move firebase auth in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
+import './firebase'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import { Quasar } from 'quasar'
 import quasarUserOptions from './quasar-user-options'
-import './firebase'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
-createApp(App).use(Quasar, quasarUserOptions).use(store).use(router).mount('#app')
+const auth = getAuth()
+let appRendered = false
+
+onAuthStateChanged(auth, (user) => {
+  if (user) store.commit('SET_USER', user)
+
+  if (!appRendered) {
+    createApp(App).use(Quasar, quasarUserOptions).use(store).use(router).mount('#app')
+    appRendered = true
+  }
+})

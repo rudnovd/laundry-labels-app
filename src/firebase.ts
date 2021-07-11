e45ffc3b09ee999535e27f618456632405afef9d
rudnovd <rudnov.d@gmail.com>
2021-07-11 22:35:21 +0500

    refactor(firebase): add short initializeApp, firestore and storage references

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
-const firebaseConfig = {
+initializeApp({
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
   authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
@@ -9,19 +10,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.VUE_APP_FIREBASE_APP_ID,
   databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
-}
-
-initializeApp(firebaseConfig)
-
-const auth = getAuth()
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-    const uid = user.uid
-    // ...
-  } else {
-    // User is signed out
-    // ...
-  }
 })
+
+export const db = getFirestore()
+export const storage = getStorage()

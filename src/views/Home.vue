99e94048bbd69efff1ebbc3dfba07dc13219ac6f
rudnovd <rudnov.d@gmail.com>
2021-07-13 14:41:11 +0000

    feat(Home): replace circular to skeleton, fix styles and script

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,12 @@
 <template>
-  <section class="home-page">
-    <q-btn color="primary" icon="add" to="/create"> Add item </q-btn>
-    <section class="cards-demo">
+  <section class="home-page q-pa-sm">
+    <div class="flex justify-end q-mb-md">
+      <q-btn color="primary" icon="add" to="/create" size="sm"> Add item </q-btn>
+    </div>
+
+    <section class="laundry-cards">
       <template v-if="isLoadingUserItems">
-        <q-circular-progress indeterminate size="50px" color="lime" class="q-ma-md" />
+        <LaundryCardSkeleton v-for="skeleton in 4" :key="`skeleton-${skeleton}`" />
       </template>
       <template v-else>
         <LaundryCard v-for="userItem in userItems" :key="userItem.id" :user-item="userItem" />
@@ -14,28 +17,31 @@
 
 <script lang="ts">
 import { computed, defineComponent, onBeforeMount, ref } from 'vue'
-import LaundryCard from '@/components/cards/LaundryCard.vue'
 import { useStore } from 'vuex'
+import LaundryCardSkeleton from '@/components/cards/LaundryCardSkeleton.vue'
+import LaundryCard from '@/components/cards/LaundryCard.vue'
 
 export default defineComponent({
   name: 'Home',
   components: {
+    LaundryCardSkeleton,
     LaundryCard,
   },
   setup() {
     const store = useStore()
 
-    const isLoadingUserItems = ref(true)
-
+    const isLoadingUserItems = ref(false)
     const userItems = computed(() => store.state.items)
 
     onBeforeMount(() => {
-      store.dispatch('getItems').finally(() => (isLoadingUserItems.value = false))
+      if (!userItems.value.length) {
+        isLoadingUserItems.value = true
+        store.dispatch('getItems').finally(() => (isLoadingUserItems.value = false))
+      }
     })
 
     return {
       isLoadingUserItems,
-
       userItems,
     }
   },
@@ -43,11 +49,7 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.home-page {
-  padding: 0 8px;
-}
-
-.cards-demo {
+.laundry-cards {
   display: grid;
   grid-template-columns: 100%;
   gap: 1rem;

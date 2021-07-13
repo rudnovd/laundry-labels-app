57c45305944e61d05616041649dd063c7fcf01b1
rudnovd <rudnov.d@gmail.com>
2021-07-13 14:43:44 +0000

    refactor: remove unused components, comments

--- src/quasar-user-options.ts
+++ src/quasar-user-options.ts
@@ -3,7 +3,6 @@ import '@quasar/extras/roboto-font/roboto-font.css'
 import '@quasar/extras/material-icons/material-icons.css'
 import { Notify } from 'quasar'
 
-// To be used on app.use(Quasar, { ... })
 export default {
   config: {},
   plugins: {

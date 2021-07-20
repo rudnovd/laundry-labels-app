1688929984cec3ed4daf299aa2783327ff81bb2e
rudnovd <rudnov.d@gmail.com>
2021-07-20 19:49:24 +0500

    feat(views): add category filter in 'Home' page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,22 @@
 <template>
   <section class="home-page q-pa-sm">
-    <div class="flex justify-end q-mb-md">
-      <q-btn color="primary" icon="add" to="/create" size="sm"> Add item </q-btn>
+    <div class="flex justify-between items-center q-mb-md">
+      <q-select
+        v-model="currentCategory.value"
+        class="category-select"
+        :disable="isCategoriesLoading || isUserItemsLoading"
+        :loading="isCategoriesLoading || isUserItemsLoading"
+        :options="laundryLabelsTypes"
+        option-value="code"
+        option-label="value"
+        dense
+      />
+
+      <q-btn color="primary" icon="add" to="/create"> Add item </q-btn>
     </div>
 
     <section class="laundry-cards">
-      <template v-if="isLoadingUserItems">
+      <template v-if="isUserItemsLoading">
         <LaundryCardSkeleton v-for="skeleton in 4" :key="`skeleton-${skeleton}`" />
       </template>
       <template v-else>
@@ -16,7 +27,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, onBeforeMount, ref } from 'vue'
+import { computed, defineComponent, onBeforeMount, reactive, ref } from 'vue'
 import { useStore } from 'vuex'
 import LaundryCardSkeleton from '@/components/cards/LaundryCardSkeleton.vue'
 import LaundryCard from '@/components/cards/LaundryCard.vue'
@@ -30,19 +41,38 @@ export default defineComponent({
   setup() {
     const store = useStore()
 
-    const isLoadingUserItems = ref(false)
     const userItems = computed(() => store.state.items)
+    const laundryLabelsTypes = computed(() => [
+      { code: null, value: 'All categories' },
+      ...store.state.laundryLabelsOptions.types,
+    ])
+
+    const isUserItemsLoading = ref(false)
+    const isCategoriesLoading = ref(false)
+    const currentCategory = reactive({
+      code: null,
+      value: 'All categories',
+    })
 
     onBeforeMount(() => {
+      if (laundryLabelsTypes.value.length === 1) {
+        isCategoriesLoading.value = true
+        store.dispatch('getItemsTypes').finally(() => (isCategoriesLoading.value = false))
+      }
+
       if (!userItems.value.length) {
-        isLoadingUserItems.value = true
-        store.dispatch('getItems').finally(() => (isLoadingUserItems.value = false))
+        isUserItemsLoading.value = true
+        store.dispatch('getItems').finally(() => (isUserItemsLoading.value = false))
       }
     })
 
     return {
-      isLoadingUserItems,
       userItems,
+      laundryLabelsTypes,
+      isCategoriesLoading,
+
+      isUserItemsLoading,
+      currentCategory,
     }
   },
 })
@@ -53,5 +83,9 @@ export default defineComponent({
   display: grid;
   grid-template-columns: 100%;
   gap: 1rem;
+}
+
+.category-select {
+  width: 150px;
 }
 </style>

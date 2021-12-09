63ee7dc4f2a5f5269ae577971d14223d9ad38d23
Daniil Rudnov <rudnov.d@gmail.com>
2021-12-09 23:57:25 +0500

    chore: add Sentry

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import '@/registerServiceWorker'
+import { Integrations } from '@sentry/tracing'
+import * as Sentry from '@sentry/vue'
 import { createPinia } from 'pinia'
 import { Quasar } from 'quasar'
 import { createApp } from 'vue'
@@ -6,4 +8,19 @@ import App from './App.vue'
 import quasarUserOptions from './quasar-user-options'
 import router from './router'
 
-createApp(App).use(Quasar, quasarUserOptions).use(createPinia()).use(router).mount('#app')
+const app = createApp(App)
+app.use(Quasar, quasarUserOptions).use(createPinia()).use(router).mount('#app')
+
+if (process.env.VUE_APP_SENTRY_DSN) {
+  Sentry.init({
+    app,
+    dsn: process.env.VUE_APP_SENTRY_DSN,
+    integrations: [
+      new Integrations.BrowserTracing({
+        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+        tracingOrigins: ['localhost', /^\//],
+      }),
+    ],
+    tracesSampleRate: 1.0,
+  })
+}

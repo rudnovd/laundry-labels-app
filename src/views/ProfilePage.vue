0ee306befdd6387f13c958733cddf05b04968717
Daniil Rudnov <rudnov.d@gmail.com>
2022-01-05 00:13:40 +0500

    feat(ProfilePage): add offline; move buttons from welcome page
    Add offline mode toggle
    
     Move login and registration button from
    welcome page
    
     refactor PWA install button

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,5 +1,11 @@
 <template>
   <section class="profile-page q-pa-sm flex justify-center">
+    <div>
+      <q-toggle v-model="offlineMode" :disable="!isOnline" color="green" label="Offline mode" left-label />
+      <q-btn icon="help_outline" flat dense @click="showOfflineModeModal" />
+    </div>
+    <q-btn v-if="!user" color="primary" label="Login" icon="login" to="/login" />
+    <q-btn v-if="!user" color="primary" label="Registration" icon="person" to="/registration" />
     <q-btn v-if="user" color="primary" label="Logout" icon="logout" @click="callLogoutDialog" />
     <q-btn v-if="showInstallButton" color="primary" label="Install app" @click="installApp" />
     App version: {{ appVersion }}
@@ -7,13 +13,11 @@
 </template>
 
 <script lang="ts">
+import { setUserSettings } from '@/localstorage'
 import { useStore } from '@/store'
 import { useQuasar } from 'quasar'
-import { computed, defineComponent, ref } from 'vue'
+import { computed, defineComponent } from 'vue'
 import { useRouter } from 'vue-router'
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-let installEvent: any
 
 export default defineComponent({
   name: 'ProfilePage',
@@ -22,23 +26,17 @@ export default defineComponent({
     const store = useStore()
     const router = useRouter()
 
-    const user = computed(() => store.user)
-    const showInstallButton = ref(false)
-
-    window.addEventListener('beforeinstallprompt', (event) => {
-      /* eslint-disable no-console */
-      console.log('beforeinstallprompt called')
-      event.preventDefault()
-      installEvent = event
-      showInstallButton.value = true
+    const isOnline = computed(() => store.isOnline)
+    const user = computed(() => store.user._id)
+    const install = computed(() => store.options.install)
+    const offlineMode = computed({
+      get: () => store.offlineMode,
+      set: (value) => {
+        store.offlineMode = value
+        setUserSettings({ offlineMode: value })
+      },
     })
-
-    window.addEventListener('appinstalled', () => {
-      showInstallButton.value = false
-      installEvent = null
-      /* eslint-disable no-console */
-      console.log('PWA was installed')
-    })
+    const showInstallButton = computed(() => store.options.install.showInstallButton)
 
     const callLogoutDialog = () => {
       $q.dialog({
@@ -54,20 +52,31 @@ export default defineComponent({
               type: 'positive',
               message: 'Sign out successfully',
             })
-            router.push('/welcome')
+            router.push('/')
           })
           .finally(() => $q.loading.hide())
       })
     }
 
-    const installApp = () => installEvent.prompt()
+    const showOfflineModeModal = () => {
+      $q.dialog({
+        message: 'All items store in your device. They can be synced with server if you have account.',
+      })
+    }
+
+    const installApp = () => {
+      install.value.event?.prompt()
+    }
 
     return {
       user,
       showInstallButton,
+      isOnline,
+      offlineMode,
       appVersion: import.meta.env.__APP_VERSION__,
 
       callLogoutDialog,
+      showOfflineModeModal,
       installApp,
     }
   },

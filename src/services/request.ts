677d366bce68bcec1855ee66d3c0ce3ed1b85f81
Daniil Rudnov <rudnov.d@gmail.com>
2022-01-04 23:38:42 +0500

    feat(request): migrate from Axios to Ky

--- src/services/request.ts
+++ src/services/request.ts
@@ -1,78 +1,79 @@
+import type { UserRefreshTokenResponse } from '@/interfaces/user'
 import router from '@/router'
 import { useStore } from '@/store'
-import axios from 'axios'
 import dayjs from 'dayjs'
 import type { JwtPayload } from 'jwt-decode'
 import jwt_decode from 'jwt-decode'
+import ky from 'ky'
 import { LocalStorage } from 'quasar'
 
 let isRefreshTokenRequestStarted = false
-const publicRoutes = [{ path: /^\/api\/auth\/.*/ }, { path: /^\/upload\/items\/.*/, methods: ['GET'] }]
+const publicRoutes = [{ path: /\/api\/auth\/.*/ }, { path: /\/upload\/items\/.*/, methods: ['GET'] }]
 
 async function getAccessToken(): Promise<string> {
   try {
     let accessToken = LocalStorage.getItem('accessToken')?.toString()
+
+    // get new token
     if (!accessToken) {
-      const auth = await axios.post('/api/auth/refreshtoken', {}, { withCredentials: true })
-      accessToken = auth.data.accessToken
+      const auth = await ky.post('/api/auth/refreshtoken').json<UserRefreshTokenResponse>()
+      accessToken = auth.refreshToken
     } else {
       const now = dayjs().unix()
-      const jwtPayload = jwt_decode(accessToken) as JwtPayload
-      if (!isRefreshTokenRequestStarted && jwtPayload.exp && jwtPayload.exp < now + 15) {
+      const MAX_REQUEST_TIME_SECONDS = 10
+      const jwtPayload: JwtPayload = jwt_decode(accessToken)
+
+      // if current token expired soon get new token
+      if (!isRefreshTokenRequestStarted && jwtPayload.exp && jwtPayload.exp < now + MAX_REQUEST_TIME_SECONDS) {
         isRefreshTokenRequestStarted = true
-        const auth = await axios.post('/api/auth/refreshtoken', {}, { withCredentials: true })
-        accessToken = auth.data.accessToken
+        const auth = await ky.post('/api/auth/refreshtoken').json<UserRefreshTokenResponse>()
+        console.log(auth)
+        accessToken = auth.refreshToken
       }
     }
 
-    isRefreshTokenRequestStarted = false
-    return accessToken || ''
+    return accessToken
   } catch (error) {
     return ''
+  } finally {
+    isRefreshTokenRequestStarted = false
   }
 }
 
-const request = axios.create({
-  validateStatus: (status) => status < 500,
+const request = ky.create({
   timeout: 10000,
-})
+  hooks: {
+    beforeRequest: [
+      async (request) => {
+        const isPublicRoute = publicRoutes.find((publicRoute) => {
+          const isMatchUrl = request.url?.match(publicRoute.path)
+          const isRouteMethodAllowed = publicRoute.methods ? publicRoute.methods.indexOf(request.method) > -1 : true
+          return isMatchUrl && isRouteMethodAllowed
+        })
+        if (isPublicRoute) return
 
-request.interceptors.request.use(async (config) => {
-  // do nothing if public api route
-  if (
-    publicRoutes.find((route) => {
-      const isMatchUrl = config.url?.match(route.path)
-      const isRouteMethod = route.methods && config.method ? route.methods.indexOf(config.method) > -1 : true
-      return isMatchUrl && isRouteMethod
-    })
-  )
-    return config
-
-  const accessToken = await getAccessToken()
-  return {
-    ...config,
-    headers: {
-      common: {
-        Authorization: `Bearer ${accessToken}`,
+        const accessToken = await getAccessToken()
+        request.headers.set('Authorization', `Bearer ${accessToken}`)
       },
-    },
-  }
-})
+    ],
+    afterResponse: [
+      async (_request, _options, response) => {
+        // validate errors with code < 500
+        if (response.status >= 500) return
 
-request.interceptors.response.use(
-  (response) => {
-    if (response.status === 401) {
-      const store = useStore()
-      store.user = {}
-      router.push('/welcome')
-    } else if (response.data.error) {
-      // eslint-disable-next-line
-      console.log(response.data.error)
-    }
-
-    return response
+        // if unauthorized error
+        if (response.status === 401) {
+          const store = useStore()
+          store.user = {}
+          router.push('/login')
+        } else if (!response.ok) {
+          // throw { error: { name: string; message: string } } from server
+          throw await response.json()
+        }
+      },
+    ],
   },
-  (error) => Promise.reject(error)
-)
+  throwHttpErrors: false,
+})
 
 export default request

4163b1a2f3d4a35cd681492a3590de678c712a04
Daniil Rudnov <rudnov.d@gmail.com>
2022-01-14 18:13:46 +0500

    refactor: move quasar options in main file

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,26 @@
-import '@/registerServiceWorker'
 import '@/styles/main.scss'
 import '@quasar/extras/material-icons/material-icons.css'
 import '@quasar/extras/roboto-font/roboto-font.css'
-import { Quasar } from 'quasar'
+import type { QuasarPluginOptions } from 'quasar'
+import { Dialog, Loading, LocalStorage, Notify, Quasar } from 'quasar'
 import 'quasar/src/css/index.sass'
 import { createApp } from 'vue'
 import App from './App.vue'
-import quasarUserOptions from './quasar-user-options'
 import router from './router'
 import { store } from './store'
+import './styles/quasar.scss'
+
+const quasarOptions: Partial<QuasarPluginOptions> = {
+  plugins: {
+    Notify,
+    Loading,
+    LocalStorage,
+    Dialog,
+  },
+}
 
 const app = createApp(App)
-app.use(Quasar, quasarUserOptions).use(store).use(router).mount('#app')
+app.use(Quasar, quasarOptions).use(store).use(router).mount('#app')
 
 // if (process.env.VITE_APP_SENTRY_DSN) {
 //   Sentry.init({

30074221603a37a9dda6c9c2ee839702c04b8dfe
Daniil Rudnov <rudnov.d@gmail.com>
2022-01-14 18:17:59 +0500

    feat(serviceworker): cache all user images

--- src/sw.ts
+++ src/sw.ts
@@ -1,11 +1,10 @@
+import { CacheableResponsePlugin } from 'workbox-cacheable-response'
 import { clientsClaim } from 'workbox-core'
 import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
 import { NavigationRoute, registerRoute } from 'workbox-routing'
-import { CacheFirst } from 'workbox-strategies'
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies'
 
 declare let self: ServiceWorkerGlobalScope
-
-clientsClaim()
 
 // self.__WB_MANIFEST is default injection point
 precacheAndRoute(self.__WB_MANIFEST)
@@ -15,17 +14,23 @@ cleanupOutdatedCaches()
 
 // to allow work offline
 registerRoute(new NavigationRoute(createHandlerBoundToURL('index.html')), new CacheFirst())
-self.skipWaiting()
 
-// cache api requests example
-// registerRoute(
-//   ({ url }) => url.pathname.startsWith('/api/'),
-//   new StaleWhileRevalidate({
-//     cacheName: 'api-cache',
-//     plugins: [
-//       new CacheableResponsePlugin({
-//         statuses: [200],
-//       }),
-//     ],
-//   })
-// )
+// cache images
+registerRoute(
+  ({ url }) => url.pathname.startsWith('/upload/items'),
+  new StaleWhileRevalidate({
+    cacheName: 'upload-items-images-cache',
+    fetchOptions: {
+      method: 'get',
+    },
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [200],
+      }),
+    ],
+  })
+)
+
+// always update service worker
+self.skipWaiting()
+clientsClaim()

5ee5a85815f01ba799e083909fac08b4ad4b5e60
Daniil Rudnov <rudnov.d@gmail.com>
2022-01-17 21:56:27 +0500

    feat(db): add itemsImages table

--- src/db.ts
+++ src/db.ts
@@ -5,12 +5,19 @@ import type { Item } from './interfaces/item'
 export class MySubClassedDexie extends Dexie {
   items!: Table<Item>
   offlineItems!: Table<Item>
+  itemsImages!: Table<{
+    _id: string
+    itemId: string | null
+    image: Blob
+    imageUrl: string
+  }>
 
   constructor() {
     super('main')
     this.version(1).stores({
-      items: '++_id, createdAt',
-      offlineItems: '++_id, createdAt',
+      items: '++_id, createdAt, updatedAt',
+      offlineItems: '++_id, createdAt, updatedAt',
+      itemsImages: '++_id, itemId, imageUrl',
     })
   }
 }

0d59a0dce4345a66506d9bc2e33c9bbf52d894a1
rudnovd <rudnov.d@gmail.com>
2022-05-29 22:58:55 +0500

    chore: move env.d.ts in root

--- env.d.ts
+++ env.d.ts
@@ -9,13 +9,6 @@ interface ImportMeta {
   readonly env: ImportMetaEnv
 }
 
-declare module '*.vue' {
-  import { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
-
 declare module '@hcaptcha/vue3-hcaptcha' {
   import Vue from 'vue'
 

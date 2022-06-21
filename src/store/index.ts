82cc102dbcbf125c6e2045e3c2214931cb59a18f
rudnovd <rudnov.d@gmail.com>
2022-06-22 00:02:43 +0500

    refactor(store): refactor error handling

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,18 +3,31 @@ import { Notify } from 'quasar'
 
 const pinia = createPinia()
 
+interface ErrorResponse {
+  name: string
+  message: string
+}
+
 pinia.use(({ store }) => {
-  store.$onAction(({ onError }) => {
+  store.$onAction(({ name, args, onError }) => {
     // catch errors for all store actions
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    onError((response: any) => {
-      console.error(`${response.name}: ${response.message}`)
+    onError((e) => {
+      const error: ErrorResponse = e as ErrorResponse
+
+      if (import.meta.env.DEV) {
+        console.warn(`Failed action "${name}" in store "${store.$id}" with args "${JSON.stringify(args)}".`)
+      }
+
+      if (!error.name || !error.message) throw new Error()
+
+      console.error(`${error.name}: ${error.message}`)
+
       Notify.create({
         type: 'negative',
-        message: response.message,
+        message: error.message,
         timeout: 5000,
       })
-      throw new Error(response.message)
+      throw new Error(error.message)
     })
   })
 })

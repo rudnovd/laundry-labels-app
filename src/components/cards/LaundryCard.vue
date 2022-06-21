124f4e293a21c54adb87deadb2f155b3928faafa
rudnovd <rudnov.d@gmail.com>
2022-06-22 00:09:55 +0500

    refactor(LaundryCard): refactor component

--- src/components/cards/LaundryCard.vue
+++ src/components/cards/LaundryCard.vue
@@ -1,33 +1,32 @@
 <template>
   <!-- TODO: v-ripple error (https://github.com/quasarframework/quasar/issues/13154)  -->
-  <div class="laundry-card" @click="router.push(`/item/${item._id}`)">
-    <q-img
-      :class="{ 'no-image': !item.images.length }"
-      :src="item.images && item.images[0]"
-      loading="lazy"
-      decoding="async"
-      height="100%"
-      :alt="item._id"
-    />
+  <q-card class="laundry-card" flat bordered @click="router.push(`/item/${item._id}`)">
+    <q-card-section horizontal class="full-height">
+      <q-img v-if="item.images.length" class="col-5" :src="item.images[0]" />
 
-    <section class="laundry-card-body">
-      <ul>
-        <li v-for="icon in iconsValues.slice(0, 6)" :key="icon._id">
-          <q-icon tag="li" :name="icon.icon" class="laundry-card-icon" size="2em" />
-        </li>
-      </ul>
+      <q-card-section
+        :class="item.images.length ? 'col-7' : 'col-12'"
+        class="q-pa-sm column justify-between laundry-card-body"
+      >
+        <div class="icons row full-width" :class="!item.images.length && 'no-image'">
+          <ul>
+            <li v-for="icon in iconsValues.slice(0, item.images.length ? 6 : 8)" :key="icon._id">
+              <q-icon tag="li" :name="icon.icon" class="laundry-card-icon" size="2.2em" />
+            </li>
+          </ul>
+        </div>
 
-      <div v-if="item.tags.length" class="laundry-card-body-footer">
-        <q-chip v-for="tag in item.tags" :key="tag">{{ tag }}</q-chip>
-      </div>
-    </section>
-  </div>
+        <div v-if="item.tags.length" class="row no-wrap overflow-auto full-width q-pb-sm">
+          <q-chip v-for="tag in item.tags" :key="tag">{{ tag }}</q-chip>
+        </div>
+      </q-card-section>
+    </q-card-section>
+  </q-card>
 </template>
 
 <script setup lang="ts">
 import { laundryIcons } from '@/assets/laundryIcons'
 import type { Item } from '@/interfaces/item'
-import { defineProps } from 'vue'
 import { useRouter } from 'vue-router'
 
 const props = defineProps<{
@@ -41,42 +40,20 @@ const iconsValues = laundryIcons.filter((icon) => props.item.icons.indexOf(icon.
 
 <style lang="scss" scoped>
 .laundry-card {
-  background: rgb(255, 255, 255);
-  border-radius: 4px;
-  box-shadow: 0 1px 5px rgb(0 0 0 / 20%), 0 2px 2px rgb(0 0 0 / 14%), 0 3px 1px -2px rgb(0 0 0 / 12%);
-  position: relative;
   width: 100%;
-  display: grid;
-  grid-template-columns: 3fr 4fr;
-  grid-template-rows: 150px;
   user-select: none;
   max-width: 500px;
+  height: 150px;
 }
 
-.laundry-card-body {
-  padding: 8px;
-  display: grid;
-  gap: 12px;
-
-  // span:first-child {
-  //   display: -webkit-box;
-  //   -webkit-line-clamp: 1;
-  //   -webkit-box-orient: vertical;
-  //   text-overflow: ellipsis;
-  //   overflow-wrap: break-word;
-  //   overflow: hidden;
-  // }
-
+.icons {
   ul {
+    width: 100%;
     list-style-type: none;
     margin: 0;
     padding: 0;
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: 32px 32px;
-    grid-auto-rows: 0;
-    overflow-y: hidden;
-    max-height: calc(64px + 0.5rem);
+    grid-template-columns: repeat(3, minmax(32px, 1fr));
     gap: 0.5rem;
   }
 
@@ -88,25 +65,11 @@ const iconsValues = laundryIcons.filter((icon) => props.item.icons.indexOf(icon.
     border-radius: 4px;
     height: 32px;
   }
+}
 
-  li img {
-    font-size: 1.75rem;
+.icons.no-image {
+  ul {
+    grid-template-columns: repeat(auto-fit, minmax(64px, 1fr));
   }
-}
-
-.laundry-card-body-footer {
-  display: flex;
-  overflow-x: auto;
-  overflow-y: hidden;
-  height: 48px;
-}
-
-.no-image {
-  background: $grey-4;
-}
-
-.laundry-card-icon {
-  width: 100%;
-  height: 100%;
 }
 </style>

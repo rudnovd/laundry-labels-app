d648c694222c3d12e4eaa7f064932c45527b20ae
rudnovd <rudnov.d@gmail.com>
2022-10-10 21:59:57 +0500

    fix(types): fix Hcaptcha types

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -52,7 +52,7 @@ watch(user, () => user.value?._id && router.push('/'))
 
 const email = ref('')
 const password = ref('')
-const captchaForm = ref<VueHcaptcha>()
+const captchaForm = ref()
 const captchaIsVerified = ref(false)
 const captchaVerificationToken = ref('')
 

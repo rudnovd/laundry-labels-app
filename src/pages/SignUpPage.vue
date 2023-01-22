9c89d4f37906df69896a7d8ddf2c4a794ba6023c
rudnovd <rudnov.d@gmail.com>
2023-01-22 16:01:10 +0500

    refactor(pages): rename RegistrationPage

--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -1,9 +1,9 @@
 <template>
-  <section class="q-pa-sm column items-center">
+  <section class="registration-page q-pa-sm">
     <h1 class="text-h6">Sign up</h1>
 
     <q-form
-      class="registration-form column items-center"
+      class="registration-form"
       autocorrect="off"
       autocapitalize="off"
       autocomplete="off"
@@ -12,18 +12,16 @@
     >
       <q-input
         v-model="email"
-        class="q-mb-sm"
         type="email"
         label="Email"
-        maxlength="128"
+        maxlength="320"
         filled
         dense
         lazy-rules
-        :rules="[(val: any) => (val && val.length > 0) || 'Please type something']"
+        :rules="[(v) => (v && v.length) || 'The email field is not filled']"
       />
       <q-input
         v-model="password"
-        class="q-mb-sm"
         type="password"
         label="Password"
         maxlength="64"
@@ -31,14 +29,12 @@
         filled
         dense
         lazy-rules
-        :rules="[(val: any) => (val && val.length >= 6) || 'Please use minimum 6 characters']"
+        :rules="[(v) => (v && v.length >= 6) || 'Please use minimum 6 characters']"
       />
 
-      <VueHcaptcha v-if="showCaptcha" ref="captchaForm" class="q-mb-sm" :sitekey="sitekey" @verify="onVerifyCaptcha" />
-
       <q-btn label="Sign up" type="submit" color="primary" />
+      <VueHcaptcha v-if="showCaptcha" ref="captchaForm" :sitekey="sitekey" @verify="onVerifyCaptcha" />
     </q-form>
-
     <div class="row q-mt-lg">
       <div class="col-xs-12">Already registered? <router-link to="/signin">Sign in</router-link></div>
     </div>
@@ -52,7 +48,7 @@ import { throttle, useQuasar } from 'quasar'
 import { computed, ref, watch } from 'vue'
 import { useRouter } from 'vue-router'
 
-const $q = useQuasar()
+const { notify, loading } = useQuasar()
 const userStore = useUserStore()
 const router = useRouter()
 
@@ -73,14 +69,14 @@ const onVerifyCaptcha = (token: string) => {
 
 const onSubmit = throttle(() => {
   if (import.meta.env.PROD && !captchaIsVerified.value) {
-    return $q.notify({
+    return notify({
       type: 'negative',
       message: 'Captcha required',
       timeout: 5000,
     })
   }
 
-  $q.loading.show()
+  loading.show()
   userStore
     .signUp({
       email: email.value,
@@ -88,14 +84,14 @@ const onSubmit = throttle(() => {
       token: captchaVerificationToken.value,
     })
     .then(() => {
-      $q.notify({
+      notify({
         type: 'positive',
         message: 'Sign up successfully',
       })
       router.push('/')
     })
     .catch(() => captchaForm.value?.reset())
-    .finally(() => $q.loading.hide())
+    .finally(() => loading.hide())
 }, 5000)
 
 const sitekey = import.meta.env.VITE_APP_CAPTCHA_KEY
@@ -103,7 +99,17 @@ const showCaptcha = import.meta.env.PROD
 </script>
 
 <style lang="scss" scoped>
-.registration-form > label {
-  min-width: 250px;
+.registration-page {
+  display: grid;
+  place-items: center;
+  height: 100%;
+}
+
+.registration-form {
+  display: grid;
+  grid-template-columns: 100%;
+  gap: 0.5rem;
+  grid-auto-flow: row;
+  width: 300px;
 }
 </style>

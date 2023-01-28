76dbee9b85925d7bf035ef813a3d3b7d8be2fffd
rudnovd <rudnov.d@gmail.com>
2023-01-29 02:17:23 +0500

    feat(SelectItemTags): update component features

--- src/components/SelectItemTags.vue
+++ src/components/SelectItemTags.vue
@@ -1,24 +1,30 @@
 <template>
   <q-select
+    ref="select"
     :model-value="modelValue"
     class="q-mb-md"
     outlined
-    clearable
     label="Item tags"
     use-input
     multiple
-    hide-dropdown-icon
     new-value-mode="add-unique"
-    :placeholder="!modelValue.length ? 'Input or select tags from list' : ''"
+    placeholder="Input or select tag from list"
     :max-values="10"
     :options="standardTags"
     use-chips
     autocomplete="off"
-    @update:model-value="emit('update:modelValue', $event)"
-  />
+    @update:model-value="onUpdateModelValue"
+    @input-value="onInputValue"
+  >
+    <template #append>
+      <q-btn v-show="showAddValueButton" round color="positive" size="xs" icon="check" @click="onAddTag" />
+    </template>
+  </q-select>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+
 withDefaults(
   defineProps<{
     modelValue: Array<string>
@@ -27,17 +33,80 @@ withDefaults(
 )
 const emit = defineEmits(['update:modelValue'])
 
+const select = ref()
+const showAddValueButton = ref(false)
+const inputValue = ref('')
+
+const onUpdateModelValue = (value: Array<string>) => {
+  if (!value[value.length - 1].trim()) {
+    if (showAddValueButton.value) {
+      showAddValueButton.value = false
+    }
+    return
+  }
+
+  if (showAddValueButton.value) {
+    showAddValueButton.value = false
+    select.value.updateInputValue('')
+    inputValue.value = ''
+  }
+
+  value[value.length - 1] = value[value.length - 1].trim()
+  emit('update:modelValue', value)
+}
+
+const onInputValue = (value: string) => {
+  inputValue.value = value
+  if (!showAddValueButton.value) {
+    showAddValueButton.value = true
+  }
+}
+
+const onAddTag = () => {
+  if (!inputValue.value) {
+    return
+  }
+
+  select.value.add(inputValue.value.trim(), true)
+  select.value.updateInputValue('')
+  inputValue.value = ''
+  showAddValueButton.value = false
+}
+
 const standardTags = [
   'black',
-  'white',
-  'red',
-  'yellow',
+  'blue',
+  'brown',
   'green',
-  'jeans',
-  't-shirt',
+  'grey',
+  'orange',
+  'pink',
+  'purple',
+  'red',
+  'white',
+  'yellow',
+  'boots',
+  'cap',
+  'coat',
   'dress',
+  'frock',
+  'fur coat',
+  'gloves',
+  'jacket',
+  'jeans',
+  'jumper',
+  'leggings',
+  'overalls',
+  'pyjamas',
+  'pants',
+  'scarf',
+  'shirt',
+  'shoes',
+  'skirt',
+  'socks',
+  'suit',
   'sweater',
-  'shorts',
-  'skirts',
+  't-shirt',
+  'underwear',
 ]
 </script>

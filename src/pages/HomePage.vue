d5ddd4ba19b20fa9381f82d2ee34a9295e6287ef
rudnovd <rudnov.d@gmail.com>
2023-02-06 00:25:47 +0500

    feat(HomePage): update styles

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,19 +1,27 @@
 <template>
-  <q-page class="welcome-page q-pa-sm">
-    <q-btn color="primary" label="Sign in" icon="login" :to="{ name: 'Sign in' }" />
-    <q-btn color="primary" label="Sign up" icon="person" :to="{ name: 'Sign up' }" />
+  <q-page class="home-page bg-positive text-weight-bold">
+    <section>
+      <h1 class="text-h4">Laundry Labels App</h1>
+      <q-btn color="primary" label="Sign in" icon="login" :to="{ name: 'Sign in' }" />
+      <q-btn color="primary" label="Sign up" icon="person" :to="{ name: 'Sign up' }" />
+    </section>
   </q-page>
 </template>
 
 <style lang="scss" scoped>
-.buttons-section {
-  display: grid;
-  gap: 1rem;
+.home-page {
+  display: flex;
+  align-items: center;
   justify-content: center;
 
-  button,
-  a {
-    max-width: 300px;
+  & > section:first-child {
+    display: grid;
+    gap: 1rem;
+    justify-content: center;
+
+    a {
+      max-width: 300px;
+    }
   }
 }
 </style>

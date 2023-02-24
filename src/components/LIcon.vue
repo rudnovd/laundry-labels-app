d0d5c53680b78929b48f4b1fc44323fb3e394b06
rudnovd <rudnov.d@gmail.com>
2023-02-24 19:42:26 +0500

    feat: add component for svg icons

---
+++ src/components/LIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <component :is="icon" height="16px" width="16px" v-bind="attrs" />
+</template>
+
+<script setup lang="ts">
+import { computed, defineAsyncComponent, useAttrs } from 'vue'
+
+const props = withDefaults(
+  defineProps<{
+    icon: string
+  }>(),
+  {}
+)
+const attrs = useAttrs()
+
+const icon = computed(() => {
+  return defineAsyncComponent(() => import(`@/assets/icons/${props.icon}.svg`))
+})
+</script>

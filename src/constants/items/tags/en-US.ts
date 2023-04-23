04da0597351977b8352c391cfb1b024c771f7237
rudnovd <rudnov.d@gmail.com>
2023-04-23 15:37:24 +0500

    feat(constants): move item tags in constants

---
+++ src/constants/items/tags/en-US.ts
@@ -0,0 +1,26 @@
+export default [
+  {
+    group: 'Colors',
+    items: ['black', 'blue', 'brown', 'gray', 'green', 'orange', 'pink', 'purple', 'red', 'yellow', 'white'],
+  },
+  {
+    group: 'Tops',
+    items: ['shirt', 't-shirt', 'hoodie', 'dress', 'cardigan', 'jacket', 'jumper', 'overalls', 'suit', 'sweater'],
+  },
+  {
+    group: 'Bottoms',
+    items: ['jeans', 'leggings', 'pants', 'shorts', 'skirt', 'trousers'],
+  },
+  {
+    group: 'Outerwear',
+    items: ['coat', 'fur coat', 'gloves', 'cap', 'scarf'],
+  },
+  {
+    group: 'Footwear',
+    items: ['shoes', 'trainers', 'boots'],
+  },
+  {
+    group: 'Underwear and nightwear',
+    items: ['underwear', 'socks', 'pyjamas', 'thermal'],
+  },
+]

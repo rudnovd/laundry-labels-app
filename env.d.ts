0e7e7de4ddd0fbabfdaf280d9047bd1c2f402921
Daniil Rudnov <rudnov.d@gmail.com>
2024-01-03 18:34:42 +0500

    feat!: init supabase

--- env.d.ts
+++ env.d.ts
@@ -5,6 +5,8 @@ interface ImportMetaEnv {
   readonly VITE_APP_CAPTCHA_KEY: string
   readonly __APP_VERSION__: string
   readonly VITE_APP_IS_LOCAL: boolean
+  readonly VITE_APP_SUPABASE_URL: string
+  readonly VITE_APP_SUPABASE_KEY: string
 }
 
 interface ImportMeta {

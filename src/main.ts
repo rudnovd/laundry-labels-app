86d1ff2fd91567d598e6a7655d48e71150ac8bd0
Daniil Rudnov <rudnov.d@gmail.com>
2024-01-04 22:36:42 +0500

    chore: remove Quasar LocalStorage plugin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import pinia from '@/store'
 import '@/styles/main.scss'
 import '@quasar/extras/material-icons/material-icons.css'
 import type { QuasarPluginOptions } from 'quasar'
-import { Dialog, Loading, LocalStorage, Notify, Quasar } from 'quasar'
+import { Dialog, Loading, Notify, Quasar } from 'quasar'
 import 'quasar/src/css/index.sass'
 import { createApp } from 'vue'
 
@@ -13,7 +13,6 @@ const quasarOptions: Partial<QuasarPluginOptions> = {
   plugins: {
     Notify,
     Loading,
-    LocalStorage,
     Dialog,
   },
   config: {
@@ -23,5 +22,4 @@ const quasarOptions: Partial<QuasarPluginOptions> = {
   },
 }
 
-const app = createApp(App)
-app.use(Quasar, quasarOptions).use(i18n).use(pinia).use(router).mount('#app')
+createApp(App).use(Quasar, quasarOptions).use(i18n).use(pinia).use(router).mount('#app')

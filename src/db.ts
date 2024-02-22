c5849a94efe17d5d883bd307102c7ef258134710
Daniil Rudnov <rudnov.d@gmail.com>
2024-02-23 01:48:50 +0500

    feat(db): update offline database fields

--- src/db.ts
+++ src/db.ts
@@ -1,17 +1,17 @@
-import type { Item } from '@/interfaces/item'
+import type { Item } from '@/types/item'
 import Dexie, { type Table } from 'dexie'
 
 const DATABASE_VERSION = 1
 
 export class Database extends Dexie {
   offlineItems!: Table<Item>
-  upload!: Table<{ _id: string; file: File }>
+  upload!: Table<{ id: string; file: File | Blob }>
 
   constructor() {
     super('laundrylabelsapp')
     this.version(DATABASE_VERSION).stores({
-      offlineItems: '++_id, name, icons, tags, createdAt, updatedAt',
-      upload: '++_id, file',
+      offlineItems: '++id, name, symbols, tags, materials, created_at, updated_at',
+      upload: '++id, file',
     })
   }
 }

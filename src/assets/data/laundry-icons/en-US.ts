3d4019b4a9ec848b88450e60608d3c8a33a5c916
Daniil Rudnov <rudnov.d@gmail.com>
2024-02-23 00:42:21 +0500

    feat(laundry-icons): move laundry icons data from locales to data assets

---
+++ src/assets/data/laundry-icons/en-US.ts
@@ -0,0 +1,210 @@
+export default {
+  wash: {
+    group: 'washing',
+    description: 'Machine wash',
+  },
+  'mild-wash': {
+    group: 'washing',
+    description: 'Mild machine wash',
+  },
+  'very-mild-wash': {
+    group: 'washing',
+    description: 'Very mild machine wash',
+  },
+  'hand-wash': {
+    group: 'washing',
+    description: 'Hand wash',
+  },
+  'do-not-wash': {
+    group: 'washing',
+    description: 'Do not wash',
+  },
+  'wash-cold': {
+    group: 'washing',
+    description: 'Wash at or below 30°C',
+  },
+  'mild-wash-cold': {
+    group: 'washing',
+    description: 'Mild wash at or below 30°C',
+  },
+  'very-mild-wash-cold': {
+    group: 'washing',
+    description: 'Very mild wash at or below 30°C',
+  },
+  'wash-warm': {
+    group: 'washing',
+    description: 'Wash at or below 40°C',
+  },
+  'mild-wash-warm': {
+    group: 'washing',
+    description: 'Mild wash at or below 40°C',
+  },
+  'very-mild-wash-warm': {
+    group: 'washing',
+    description: 'Very mild wash at or below 40°C',
+  },
+  'wash-hot': {
+    group: 'washing',
+    description: 'Wash at or below 50°C',
+  },
+  'mild-wash-hot': {
+    group: 'washing',
+    description: 'Mild wash at or below 50°C',
+  },
+  'very-mild-wash-hot': {
+    group: 'washing',
+    description: 'Very mild wash at or below 50°C',
+  },
+  'wash-very-hot': {
+    group: 'washing',
+    description: 'Wash at or below 60°C',
+  },
+  'mild-wash-very-hot': {
+    group: 'washing',
+    description: 'Mild wash at or below 60°C',
+  },
+  'very-mild-wash-very-hot': {
+    group: 'washing',
+    description: 'Very mild wash at or below 60°C',
+  },
+  iron: {
+    group: 'ironing',
+    description: 'Iron',
+  },
+  'do-not-iron': {
+    group: 'ironing',
+    description: 'Do not iron',
+  },
+  'iron-low': {
+    group: 'ironing',
+    description: 'Iron at low temperature (max. 110°C)',
+  },
+  'iron-medium': {
+    group: 'ironing',
+    description: 'Iron at medium temperature (max. 150°C)',
+  },
+  'iron-high': {
+    group: 'ironing',
+    description: 'Iron at high temperature (max. 200°C)',
+  },
+  'iron-steam': {
+    group: 'ironing',
+    description: 'Iron with steam',
+  },
+  'iron-do-not-steam': {
+    group: 'ironing',
+    description: 'Do not steam',
+  },
+  bleach: {
+    group: 'bleaching',
+    description: 'Bleaching allowed',
+  },
+  'do-not-bleach': {
+    group: 'bleaching',
+    description: 'Do not bleach',
+  },
+  'non-chlorine-bleach': {
+    group: 'bleaching',
+    description: 'Non-chlorine bleach',
+  },
+  'tumble-dry': {
+    group: 'tumble-drying',
+    description: 'Tumble dry allowed',
+  },
+  'do-not-tumble-dry': {
+    group: 'tumble-drying',
+    description: 'Do not tumble dry',
+  },
+  'tumble-dry-low-heat': {
+    group: 'tumble-drying',
+    description: 'Tumble dry on low heat',
+  },
+  'tumble-dry-medium-heat': {
+    group: 'tumble-drying',
+    description: 'Tumble dry on medium heat',
+  },
+  'tumble-dry-high-heat': {
+    group: 'tumble-drying',
+    description: 'Tumble dry on high heat',
+  },
+  dry: {
+    group: 'natural-drying',
+    description: 'Dry',
+  },
+  'dry-in-shade': {
+    group: 'natural-drying',
+    description: 'Dry in shade',
+  },
+  'line-dry': {
+    group: 'natural-drying',
+    description: 'Line dry',
+  },
+  'line-dry-in-shade': {
+    group: 'natural-drying',
+    description: 'Line dry in shade',
+  },
+  'drip-dry': {
+    group: 'natural-drying',
+    description: 'Drip dry',
+  },
+  'drip-dry-in-shade': {
+    group: 'natural-drying',
+    description: 'Drip dry in shade',
+  },
+  'dry-flat': {
+    group: 'natural-drying',
+    description: 'Dry flat',
+  },
+  'dry-flat-in-shade': {
+    group: 'natural-drying',
+    description: 'Dry flat in shade',
+  },
+  'dry-clean': {
+    group: 'dry-cleaning',
+    description: 'Dry clean',
+  },
+  'do-not-dry-clean': {
+    group: 'dry-cleaning',
+    description: 'Do not dry clean',
+  },
+  'dry-clean-petroleum': {
+    group: 'dry-cleaning',
+    description: 'Dry clean with petroleum solvent',
+  },
+  'gentle-dry-clean-petroleum': {
+    group: 'dry-cleaning',
+    description: 'Gentle dry clean with petroleum solvent',
+  },
+  'very-gentle-dry-clean-petroleum': {
+    group: 'dry-cleaning',
+    description: 'Very gentle dry clean with petroleum solvent',
+  },
+  'dry-clean-pce': {
+    group: 'dry-cleaning',
+    description: 'Dry clean with PCE',
+  },
+  'gentle-dry-clean-pce': {
+    group: 'dry-cleaning',
+    description: 'Gentle clean with PCE',
+  },
+  'very-gentle-dry-clean-pce': {
+    group: 'dry-cleaning',
+    description: 'Very gentle clean with PCE',
+  },
+  'wet-clean': {
+    group: 'wet-cleaning',
+    description: 'Wet clean allowed',
+  },
+  'do-not-wet-clean': {
+    group: 'wet-cleaning',
+    description: 'Do not wet clean',
+  },
+  'gentle-wet-clean': {
+    group: 'wet-cleaning',
+    description: 'Gentle wet clean',
+  },
+  'very-gentle-wet-clean': {
+    group: 'wet-cleaning',
+    description: 'Very gentle wet clean',
+  },
+} as const

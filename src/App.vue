b39b56c852b9915cecf272fe3714ddbdd36394a3
Daniil Rudnov <rudnov.d@gmail.com>
2024-02-25 23:38:02 +0500

    feat(App): set app locale and user check session on before mount app

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,26 @@
 </template>
 
 <script setup lang="ts">
-import { laundryIcons } from '@/assets/laundryIcons'
-import { useLocalStorage, watchOnce } from '@vueuse/core'
-import { QSpinnerGears, useQuasar } from 'quasar'
-import { useRegisterSW } from 'virtual:pwa-register/vue'
-import { onMounted, onUnmounted } from 'vue'
+import { onBeforeMount } from 'vue'
 import { useI18n } from 'vue-i18n'
+import { useQuasar } from 'quasar'
+import { useUserStore } from '@/store/user'
 import usePwa from '@/composables/usePwa'
+import { getBrowserLocale, setLocale } from '@/utils/locale'
+
+const { loading } = useQuasar()
+const userStore = useUserStore()
 const { t } = useI18n()
+
+onBeforeMount(async () => {
+  await setLocale(getBrowserLocale())
+  loading.show({ message: t('common.authenticating') })
+  try {
+    await userStore.getSession()
+  } finally {
+    loading.hide()
+  }
+})
+
 usePwa()
 </script>

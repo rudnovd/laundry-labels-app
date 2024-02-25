499f97f2a21e6872193b17b831a60b57e7bcf3fe
Daniil Rudnov <rudnov.d@gmail.com>
2024-02-26 00:27:16 +0500

    feat(ItemsPage): update page

--- src/pages/ItemsPage.vue
+++ src/pages/ItemsPage.vue
@@ -1,64 +1,64 @@
 <template>
-  <q-page class="q-pa-sm">
-    <section v-if="items.length" class="actions">
+  <q-page class="items-page">
+    <header>
       <q-btn
         color="primary"
-        class="disabled"
         :size="isMaxItems ? 'sm' : undefined"
         :icon="isMaxItems ? undefined : 'add'"
         :to="{ name: 'Create item' }"
-        :disable="isMaxItems"
+        :disable="isMaxItems || loading.isActive"
       >
         {{ t(isMaxItems ? 'pages.items.itemsLimitReached' : 'common.add') }}
       </q-btn>
 
       <q-input
-        v-model="search"
+        v-model.trim="search"
         rounded
         outlined
         :label="t('pages.items.searchTags')"
         dense
+        :disable="loading.isActive"
         :maxlength="32"
-        @keyup.enter="onAddSearchTag"
+        @keyup.enter="searchTag"
       >
         <template #append>
-          <q-icon :class="{ invisible: !search }" name="check" @click="onAddSearchTag" />
+          <q-icon :class="{ invisible: !search }" name="check" @click="searchTag" />
         </template>
       </q-input>
-    </section>
+    </header>
 
-    <section v-if="searchTags.length" class="search-tags">
-      <q-chip class="text-white text-lowercase" color="negative" clickable @click="searchTags = []">
+    <div v-if="searchingTags.length" class="search-tags">
+      <item-tag
+        class="chip text-white text-lowercase bg-negative"
+        color="negative"
+        clickable
+        @click="searchingTags = []"
+      >
         <q-icon class="q-mr-xs" name="delete" size="16px" />
         {{ t('common.clear') }}
-      </q-chip>
-      <q-chip v-for="tag in searchTags" :key="tag" removable @remove="onRemoveSearchTag(tag)">
+      </item-tag>
+      <button v-for="tag in searchingTags" :key="tag" class="chip" removable @remove="removeSearchTag(tag)">
         <span class="ellipsis">{{ tag }}</span>
-      </q-chip>
-    </section>
+      </button>
+    </div>
 
-    <section class="laundry-cards">
-      <template v-if="loading.isActive">
-        <laundry-card-skeleton v-for="skeleton in 4" :key="skeleton" />
-      </template>
-
-      <template v-else-if="items.length && !searchTags.length">
-        <laundry-card v-for="item in items" :key="item._id" :item="item" />
-      </template>
-
-      <div v-else-if="!items.length && !foundItems.length" class="flex column items-center">
-        <span class="text-h6">{{ t('pages.items.noItemsAdded') }}</span>
-        <q-btn class="q-mt-sm" color="primary" :to="{ name: 'Create item' }">{{ t('pages.items.addFirstItem') }}</q-btn>
-      </div>
-
-      <template v-else-if="foundItems.length">
-        <laundry-card v-for="item in foundItems" :key="item._id" :item="item" />
-      </template>
-
-      <div v-else-if="searchTags.length && !foundItems.length" class="flex column items-center">
-        {{ t('pages.items.noItemsWithSelectedTags') }}
-      </div>
-    </section>
+    <ul v-if="loading.isActive" class="items-cards">
+      <li v-for="skeleton in 4" :key="skeleton">
+        <laundry-card-skeleton />
+      </li>
+    </ul>
+    <ul v-else-if="foundItems.length" class="items-cards">
+      <li v-for="item in foundItems" :key="item.id">
+        <laundry-card :item="item" />
+      </li>
+    </ul>
+    <div v-else-if="searchingTags.length && !foundItems.length" class="flex column items-center">
+      {{ t('pages.items.noItemsWithSelectedTags') }}
+    </div>
+    <div v-else class="flex column items-center">
+      <span class="text-h6">{{ t('pages.items.noItemsAdded') }}</span>
+      <q-btn class="q-mt-sm" color="primary" :to="{ name: 'Create item' }">{{ t('pages.items.addFirstItem') }}</q-btn>
+    </div>
   </q-page>
 </template>
 
@@ -67,78 +67,81 @@ import { useQuasar } from 'quasar'
 import { computed, onBeforeMount, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useItemsStore } from '@/store/items'
-import LaundryCard from '@/components/cards/LaundryCard.vue'
-import LaundryCardSkeleton from '@/components/cards/LaundryCardSkeleton.vue'
+import LaundryCard from '@/components/item/cards/LaundryCard.vue'
+import LaundryCardSkeleton from '@/components/item/cards/LaundryCardSkeleton.vue'
 import useDemoMode from '@/composables/useDemoMode'
 import useItems from '@/composables/useItems'
+import { ITEMS_LIMIT } from '@/constants'
+import { defineAsyncComponent } from 'vue'
+const ItemTag = defineAsyncComponent(() => import('@/components/item/tags/ItemTag.vue'))
 
 const { loading } = useQuasar()
 const { t } = useI18n()
 const { items, getItems } = useItems()
 const itemsStore = useItemsStore()
 
-const search = ref('')
-const searchTags = ref<Array<string>>([])
+const isMaxItems = computed(() => itemsStore.items.length >= ITEMS_LIMIT)
 
-const itemsPromise = getItems()
-onBeforeMount(() => {
+onBeforeMount(async () => {
   loading.isActive = true
-
-  if (localStorage.getItem('demo')) {
-    let { showTourNotification } = useDemoMode()
-    itemsPromise.then((items) => {
-      if (!items.length) {
-        showTourNotification()
-      }
-    })
+  try {
+    const items = await getItems()
+    if (!items.length && localStorage.getItem('demo')) {
+      useDemoMode().showTourNotification()
+    }
+  } finally {
+    loading.isActive = false
   }
-  itemsPromise.finally(() => (loading.isActive = false))
 })
 
+const search = ref('')
+const searchingTags = ref<Array<string>>([])
 const foundItems = computed(() => {
+  if (!searchingTags.value.length) return items.value
   return items.value.filter((item) => {
-    for (const searchTag of searchTags.value) {
+    for (const searchTag of searchingTags.value) {
       return item.tags.includes(searchTag)
     }
   })
 })
 
-const isMaxItems = computed(() => itemsStore.items.length >= 20)
-
-const onAddSearchTag = () => {
-  if (!search.value.trim()) {
-    return
-  }
-  searchTags.value.push(search.value.trim())
+function searchTag() {
+  if (!search.value) return
+  searchingTags.value.push(search.value)
   search.value = ''
 }
 
-const onRemoveSearchTag = (tag: string) => {
-  searchTags.value = searchTags.value.filter((searchTag) => searchTag !== tag)
+function removeSearchTag(tag: string) {
+  searchingTags.value = searchingTags.value.filter((searchTag) => searchTag !== tag)
 }
 </script>
 
-<style lang="scss" scoped>
-.laundry-cards {
+<style>
+.items-page {
   display: grid;
-  grid-template-columns: 100%;
-  gap: 1rem;
+  grid-auto-rows: max-content;
+  gap: 16px;
+  padding: 8px;
+  overflow-x: hidden;
 
-  @include media-small {
-    grid-template-columns: repeat(auto-fill, minmax(360px, 1fr));
+  header {
+    display: grid;
+    grid-template-columns: auto 160px;
+    justify-content: space-between;
   }
-}
 
-.actions {
-  display: grid;
-  grid-template-columns: auto 160px;
-  justify-content: space-between;
-  margin-bottom: 1rem;
-}
+  .search-tags {
+    display: flex;
+    gap: 8px;
+    padding-bottom: 4px;
+    overflow-x: auto;
+    scrollbar-width: thin;
+  }
 
-.search-tags {
-  max-height: 215px;
-  margin-bottom: 0.5rem;
-  overflow: hidden auto;
+  .items-cards {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(calc(200px + 20dvmax), 1fr));
+    gap: 16px;
+  }
 }
 </style>

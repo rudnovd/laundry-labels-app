50e66d4ae5e6d1130740f2cc9a373334815914f2
Daniil Rudnov <rudnov.d@gmail.com>
2024-02-29 01:24:16 +0500

    feat(App): get standard symbols once when entering on item pages

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,19 @@
 </template>
 
 <script setup lang="ts">
-import { onBeforeMount } from 'vue'
+import { onBeforeMount, watch } from 'vue'
+import { useRouter } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 import { useQuasar } from 'quasar'
 import { useUserStore } from '@/store/user'
 import usePwa from '@/composables/usePwa'
 import { getBrowserLocale, setLocale } from '@/utils/locale'
+import { useItemsStore } from '@/store/items'
 
 const { loading } = useQuasar()
 const userStore = useUserStore()
 const { t } = useI18n()
+const router = useRouter()
 
 onBeforeMount(async () => {
   await setLocale(getBrowserLocale())
@@ -25,4 +28,15 @@ onBeforeMount(async () => {
 })
 
 usePwa()
+watch(
+  router.currentRoute,
+  ({ matched }) => {
+    const { getStandardSymbols, getStandardTags } = useItemsStore()
+    if (matched.some(({ path }) => path === '/items')) {
+      getStandardSymbols()
+      getStandardTags()
+    }
+  },
+  { once: true },
+)
 </script>

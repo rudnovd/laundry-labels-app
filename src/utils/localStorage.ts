50c478ef7fa9c3eebce0a9361497f904e9245661
Daniil Rudnov <rudnov.d@gmail.com>
2024-02-29 01:07:00 +0500

    refactor(utils): update localStorage composable types

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,9 +1,25 @@
-import { getBrowserLocale } from '@/i18n'
-import type { LocalStorageDemo, UserSettings } from '@/types/types'
+import type { RouteRecordName } from 'vue-router'
 import { useLocalStorage } from '@vueuse/core'
+import type { AvailableLocale } from '@/i18n'
+import { getBrowserLocale } from '@/utils/locale'
 
-export const userSettingsStorage = useLocalStorage<UserSettings>('user-settings', {
-  language: getBrowserLocale(),
+interface UserSettingsLocalStorage {
+  locale: AvailableLocale
+  autoUpdateApp: boolean
+  offlineMode: boolean
+  items: {
+    standardTagsLocale: AvailableLocale
+  }
+}
+
+interface DemoLocalStorage {
+  active: boolean
+  page: RouteRecordName | null | undefined
+  step: string | null
+}
+
+export const userSettingsStorage = useLocalStorage<UserSettingsLocalStorage>('user-settings', {
+  locale: getBrowserLocale(),
   autoUpdateApp: true,
   offlineMode: false,
   items: {
@@ -11,7 +27,7 @@ export const userSettingsStorage = useLocalStorage<UserSettings>('user-settings'
   },
 })
 
-export const demoStorage = useLocalStorage<Partial<LocalStorageDemo>>(
+export const demoStorage = useLocalStorage<Partial<DemoLocalStorage>>(
   'demo',
   {},
   { listenToStorageChanges: false, writeDefaults: false },

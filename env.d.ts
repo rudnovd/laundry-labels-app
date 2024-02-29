d684fd3c5266fd8d68eb16ed9b0af57ce9aa8a0d
Daniil Rudnov <rudnov.d@gmail.com>
2024-03-01 00:50:30 +0500

    chore: fix types

--- env.d.ts
+++ env.d.ts
@@ -1,10 +1,11 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/vue" />
+/// <reference types="vite-svg-loader" />
 
 interface ImportMetaEnv {
   readonly VITE_APP_CAPTCHA_KEY: string
   readonly __APP_VERSION__: string
-  readonly VITE_APP_IS_LOCAL: boolean
+  readonly VITE_APP_IS_LOCAL: string
   readonly VITE_APP_SUPABASE_URL: string
   readonly VITE_APP_SUPABASE_KEY: string
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
+import { createApp } from 'vue'
+import { Dialog, Loading, Notify, Quasar, type QuasarPluginOptions } from 'quasar'
 import App from '@/App.vue'
 import i18n from '@/i18n'
 import router from '@/router'
 import pinia from '@/store'
 import '@/styles/main.css'
 import '@quasar/extras/material-icons/material-icons.css'
-import type { QuasarPluginOptions } from 'quasar'
-import { Dialog, Loading, Notify, Quasar } from 'quasar'
 import 'quasar/src/css/index.sass'
-import { createApp } from 'vue'
 
 const quasarOptions: Partial<QuasarPluginOptions> = {
   plugins: {

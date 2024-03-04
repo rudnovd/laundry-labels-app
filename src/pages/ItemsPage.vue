2d5e484990803d8ef4aa1ea9e69f1dc1547f58ed
Daniil Rudnov <rudnov.d@gmail.com>
2024-03-05 00:41:49 +0500

    feat(items): add migration items functions

--- src/pages/ItemsPage.vue
+++ src/pages/ItemsPage.vue
@@ -65,6 +65,7 @@
 <script setup lang="ts">
 import { computed, onBeforeMount, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
+import { useQuasar } from 'quasar'
 import { useItemsStore } from '@/store/items'
 import LaundryCard from '@/components/item/cards/LaundryCard.vue'
 import LaundryCardSkeleton from '@/components/item/cards/LaundryCardSkeleton.vue'
@@ -72,11 +73,16 @@ import useDemoMode from '@/composables/useDemoMode'
 import useItems from '@/composables/useItems'
 import { ITEMS_LIMIT } from '@/constants'
 import { defineAsyncComponent } from 'vue'
+import { supabase } from '@/supabase'
+import { useUserStore } from '@/store/user'
+import type { Item, ItemBlank } from '@/types/item'
 const ItemTag = defineAsyncComponent(() => import('@/components/item/tags/ItemTag.vue'))
 
+const { loading, notify } = useQuasar()
 const { t } = useI18n()
 const { items, getItems } = useItems()
 const itemsStore = useItemsStore()
+const userStore = useUserStore()
 
 const isLoading = ref(false)
 const isMaxItems = computed(() => itemsStore.items.length >= ITEMS_LIMIT)
@@ -92,6 +98,29 @@ onBeforeMount(async () => {
   } finally {
     isLoading.value = false
   }
+
+  // TODO: remove after migration date is over
+  itemsStore.getMigrationItems().then((migration) => {
+    if (migration.items.length && !migration.migration_date) {
+      notify({
+        message:
+          'Welcome to the new version of the app! Please complete the migration and copy items from the old version',
+        color: 'brand',
+        timeout: 60_000,
+        actions: [
+          {
+            label: 'Skip',
+            color: 'black',
+          },
+          {
+            label: t('demo.notification.buttons.start'),
+            color: 'black',
+            handler: () => migrate(migration.items),
+          },
+        ],
+      })
+    }
+  })
 })
 
 const search = ref('')
@@ -113,6 +142,66 @@ function searchTag() {
 
 function removeSearchTag(tag: string) {
   searchingTags.value = searchingTags.value.filter((searchTag) => searchTag !== tag)
+}
+
+// TODO: remove after migration date is over
+async function migrate(items: Array<Item>) {
+  const userId = userStore.user?.id
+  if (!userId) return notify({ type: 'negative', message: 'Authorization required' })
+
+  try {
+    loading.show({ message: 'Migration', delay: 0 })
+    const itemsPromises: Array<Promise<Item>> = []
+    const photosPromises: Array<Promise<unknown>> = []
+    const photosPaths: Array<string> = []
+    for (const [index, item] of items.entries()) {
+      const newItem: Omit<ItemBlank, 'owner'> = {
+        name: item.name ?? null,
+        symbols: item.symbols ?? [],
+        photos: [],
+        materials: item.materials ?? [],
+        tags: item.tags ?? [],
+      }
+      if (item.photos.length) {
+        photosPaths[index] = `${userId}/${Date.now()}-${index}`
+        photosPromises[index] = supabase.storage.from('items').move(`migration/${item.photos[0]}`, photosPaths[index])
+        newItem.photos = [`${userId}/${Date.now()}-${index}`]
+      }
+      itemsPromises.push(itemsStore.createItem(newItem))
+    }
+    const results = await Promise.allSettled(itemsPromises)
+    await Promise.allSettled(photosPromises)
+    const createdItems: Array<Item> = []
+    for (const result of results) {
+      if (result.status === 'fulfilled') createdItems.push(result.value)
+    }
+    loading.hide()
+    if (createdItems.length !== items.length) {
+      notify({
+        type: 'negative',
+        message: `${createdItems.length} of ${items.length} migrated successfully, try migrate items with errors?`,
+        actions: [
+          {
+            label: 'Skip',
+            color: 'black',
+          },
+          {
+            label: t('demo.notification.buttons.start'),
+            color: 'black',
+            handler: () => migrate(items.filter((item) => createdItems.find(({ id }) => id !== item.id))),
+          },
+        ],
+      })
+    } else {
+      await itemsStore.updateMigrationDate()
+      notify({
+        type: 'positive',
+        message: 'Migration completed successfully',
+      })
+    }
+  } catch (error) {
+    migrate(items)
+  }
 }
 </script>
 

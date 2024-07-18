34d2ba560a61180e514716d547962b2f87b4d87c
Daniil Rudnov <rudnov.d@gmail.com>
2024-07-18 22:48:38 +0500

    feat(i18n): add ru plural rule

--- src/i18n.ts
+++ src/i18n.ts
@@ -7,6 +7,15 @@ const i18n = createI18n({
   legacy: false,
   locale: 'en-US',
   availableLocales,
+  pluralRules: {
+    ru: pluralizationRu,
+  },
 })
+
+function pluralizationRu(count: number) {
+  if (count % 10 === 1 && count % 100 !== 11) return 0
+  else if (count % 10 >= 2 && count % 10 <= 4 && (count % 100 < 10 || count % 100 >= 20)) return 1
+  return 2
+}
 
 export default i18n

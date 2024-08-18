fe5ea7c82330b00d47a1084e1832e172d0e20fc1
Daniil Rudnov <rudnov.d@gmail.com>
2024-08-18 23:40:43 +0500

    feat(constants): update items limit

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,4 +3,4 @@ export const REQUEST_THROTTLE_TIMEOUT = 5000
 
 export const MAX_ITEM_PHOTO_UNCOMPRESSED_SIZE = 15_728_640
 export const HAS_ITEMS_LIMIT = !IS_LOCAL
-export const ITEMS_LIMIT = 20
+export const ITEMS_LIMIT = 50

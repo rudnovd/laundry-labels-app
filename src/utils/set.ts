5d8f2e59f820762e4d424559b4a50ee628559153
Daniil Rudnov <rudnov.d@gmail.com>
2024-08-26 00:18:51 +0500

    refactor(utils): use native set intersection method

--- src/utils/set.ts
+++ src/utils/set.ts
@@ -1,16 +1,8 @@
-// TODO: use native set method after TS 5.5
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/intersection
-export function setIntersection<T extends string>(setA: Set<T>, setB: Set<T>): Set<T> {
-  const _intersection = new Set<T>()
-  for (const elem of setB) {
-    if (setA.has(elem)) {
-      _intersection.add(elem)
-    }
-  }
-  return _intersection
-}
+import { toRaw } from 'vue'
 
 export function isEqualSets<T extends string>(firstSet: Set<T>, secondSet: Set<T>): boolean {
-  const intersectedSet = setIntersection<T>(firstSet, secondSet)
-  return firstSet.size === secondSet.size && intersectedSet.size === firstSet.size
+  // https://github.com/vuejs/core/issues/11398
+  const localSet = toRaw(new Set(firstSet))
+  const intersectedSet = localSet.intersection(secondSet)
+  return localSet.size === secondSet.size && intersectedSet.size === localSet.size
 }
